6bd69ca6840d3f2f6bd8e3302edfcbc3460b8618
lensp <lensp@DESKTOP-364B9QF>
2020-01-06 21:49:32 +0100

    añadidos metodos de busqueda y eliminación

--- L3PAY_/src/conexion/procs.java
+++ L3PAY_/src/conexion/procs.java
@@ -22,5 +22,51 @@ public class procs {
 			
 			
 		}
-	
+		public int search(String username) {
+			
+			int positionUser=-1;
+			int i=0;
+			boolean encontrado=false;
+			
+			while(i!=NuevoUsuario.length) {
+				
+				if(NuevoUsuario[i]!=null && encontrado == false) {
+		
+					if(name.contentEquals(NuevoUsuario[i].username)) {
+						positionKid = i;
+						encontrado=true;
+					}else {
+
+						encontrado=false;
+					}
+				}
+				i++;
+			}
+			return positionUser;	
+		}
+		
+		public void remove(user){
+
+			int position = search(users.username);
+			int contador=0;
+			
+			for(int i=0; i</*Lista de users.username*/; i++) {
+				if(/*Lista de users.username*/[i]!=null) {
+					contador++;
+				}
+			}
+			
+			if(position!=-1) { 
+				/*Lista de users.username*/[position] = null;
+				reOrder();
+				next--;
+			}else if(contador==0) { 
+
+				throw new Exception("ERROR: empty list.");
+			}
+			else { 
+
+				throw new Exception("ERROR:" +/*user.username*/  + " doesnt not exist.");
+			}
+		}
 }
