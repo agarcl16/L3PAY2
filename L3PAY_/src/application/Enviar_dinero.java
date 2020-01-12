0dd5b968e92831c40d69313d376d29aee4f5ed29
usuario <usuario@DESKTOP-CEFJ8TF>
2020-01-12 17:34:01 +0100

    q4tr

--- L3PAY_/src/application/Enviar_dinero.java
+++ L3PAY_/src/application/Enviar_dinero.java
@@ -70,7 +70,7 @@ public class Enviar_dinero {
 		else {
 			//comprobamos con un if si existe el usuario
 			boolean comprobacion = controlador2.search(name1.getText());
-			if(comprobacion){
+			if(!comprobacion){
 				mensaje1.setText("error");
 			}
 			else {
@@ -93,7 +93,7 @@ public class Enviar_dinero {
 			//comprobamos con un if si existe el usuario
 			//comprobar cual está rellenadoa
 			boolean comprobacion = controlador2.search(name2.getText());
-			if(comprobacion){
+			if(!comprobacion){
 				mensaje2.setText("error");
 			}
 			else {
